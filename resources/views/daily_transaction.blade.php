94809244d7d41646a93edba20f4e9ea092352394
starno <starnleymbote@gmail.com>
2022-04-10 22:47:04 +0300

    Editted the top bar to include the user profile

--- resources/views/daily_transaction.blade.php
+++ resources/views/daily_transaction.blade.php
@@ -3,7 +3,19 @@
 @section('content')
 
 <main id="main" class="main">
-    <div class="card">
+  
+  <div class="pagetitle">
+    <h1>Dashboard</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.html">Transactions</a></li>
+        <li class="breadcrumb-item active">Daily</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
+  
+  <div class="card">
+      
         <div class="card-body">
           <h5 class="card-title">Daily Transactions</h5>
 
@@ -13,45 +25,45 @@
               <tr>
                 <th scope="col">#</th>
                 <th scope="col">Name</th>
-                <th scope="col">Position</th>
-                <th scope="col">Age</th>
-                <th scope="col">Start Date</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Received On</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <th scope="row">1</th>
                 <td>Brandon Jacob</td>
-                <td>Designer</td>
-                <td>28</td>
+                <td>0722123456</td>
+                <td>280</td>
                 <td>2016-05-25</td>
               </tr>
               <tr>
                 <th scope="row">2</th>
                 <td>Bridie Kessler</td>
-                <td>Developer</td>
-                <td>35</td>
+                <td>0722123457</td>
+                <td>350</td>
                 <td>2014-12-05</td>
               </tr>
               <tr>
                 <th scope="row">3</th>
                 <td>Ashleigh Langosh</td>
-                <td>Finance</td>
-                <td>45</td>
+                <td>0722123459</td>
+                <td>450</td>
                 <td>2011-08-12</td>
               </tr>
               <tr>
                 <th scope="row">4</th>
                 <td>Angus Grady</td>
-                <td>HR</td>
-                <td>34</td>
+                <td>0722123456</td>
+                <td>340</td>
                 <td>2012-06-11</td>
               </tr>
               <tr>
                 <th scope="row">5</th>
                 <td>Raheem Lehner</td>
-                <td>Dynamic Division Officer</td>
-                <td>47</td>
+                <td>0722123453</td>
+                <td>470</td>
                 <td>2011-04-19</td>
               </tr>
             </tbody>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -183,7 +183,7 @@
       {{-- <li class="nav-heading">Pages</li> --}}
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile.html">
+        <a class="nav-link collapsed" href="profile">
           <i class="bi bi-person"></i>
           <span>Profile</span>
         </a>
